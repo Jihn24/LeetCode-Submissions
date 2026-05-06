68016cabbaf4fa0dbaeb5b388c7d617caf6e5a27
John Summers <jsummers643@gmail.com>
2026-05-06 21:43:07 +0100

    First push to initialise repository, with an easy LeetCode problem solved.

---
+++ Submissions/Two-Sum.cs
@@ -0,0 +1,17 @@
+var solution = new Solution();
+var result = solution.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+Console.WriteLine(result[0] + ", " + result[1]);
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.Length - 1; i++) {
+            for  (int j = i + 1; j < nums.Length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return [i, j];
+                }
+            }
+        }
+        return [-1, -1];
+    }
+}
+
