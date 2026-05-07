ee2879ca257db027d7db161c394e4b75d869467b
John Summers <jsummers643@gmail.com>
2026-05-07 17:49:06 +0100

    Added comments with the challenge descriptions so far.

--- Submissions/Two-Sum.cs
+++ Submissions/Two-Sum.cs
@@ -1,3 +1,7 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
 var solution = new Solution();
 var result = solution.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
 Console.WriteLine(result[0] + ", " + result[1]);
