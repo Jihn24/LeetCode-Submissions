bcf0a0d4e7b6db8c27912a6bb58e9214a2f6ce80
John Summers <jsummers643@gmail.com>
2026-05-09 17:47:15 +0100

    Added solution to challenge 1539

---
+++ Submissions/kth-Missing.cs
@@ -0,0 +1,31 @@
+// 1539. Kth Missing Positive Number
+// Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+// Return the kth positive integer that is missing from this array.
+
+var solution = new Solution();
+var result = solution.FindKthPositive(new int[] { 2, 3, 4, 7, 11 }, 5);
+Console.WriteLine(result);
+
+public class Solution {
+    public int FindKthPositive(int[] arr, int k) {
+        int missed = 0;
+        int integer = 1;
+
+        for (int i = 0; i < arr.Length; i++) {
+            while (integer != arr[i]) {                                  
+                Console.WriteLine(integer);
+                if (missed == k) {
+                    break;
+                }
+                missed++;  
+                integer++;                               
+            }
+            if (missed == k) break;
+            integer++;
+        }
+
+        integer += k - missed;
+      
+        return integer - 1;
+    }
+}
