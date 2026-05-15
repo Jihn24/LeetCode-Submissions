c40f044a521ff8e511fd0496fb1f4aa76314bc24
John Summers <jsummers643@gmail.com>
2026-05-15 22:56:20 +0100

    Added solution to challenge 2231.

---
+++ Submissions/Swap-Match-Parity.cs
@@ -0,0 +1,47 @@
+// 2231. Largest Number After Digit Swaps by Parity
+// You are given a positive integer num. You may swap any two digits of num that have the same parity 
+// (i.e. both odd digits or both even digits).
+// Return the largest possible value of num after any number of swaps.
+
+var solution = new Solution();
+var result = solution.LargestInteger(1234); 
+Console.WriteLine(result);
+
+public class Solution {
+    public int LargestInteger(int num) {
+        string number = Convert.ToString(num);
+        string output = "";
+        List<int> evens = new List<int>();
+        List<int> odds = new List<int>();
+        bool[] parity = new bool[number.Length];
+
+        for (int i = 0; i < number.Length; i++) {
+            if (number[i] % 2 == 0) {
+                parity[i] = true;
+                evens.Add(number[i] - '0');
+            }
+            else {
+                parity[i] = false;
+                odds.Add(number[i] - '0');
+            }
+        }    
+
+        int evenIndex = evens.Count - 1;
+        int oddIndex = odds.Count - 1;
+        odds.Sort();
+        evens.Sort();
+
+        for (int i = 0; i < number.Length; i++) {
+            if (parity[i]) {
+                output += Convert.ToString(evens[evenIndex]);
+                evenIndex--;
+            }
+            else {
+                output += Convert.ToString(odds[oddIndex]);
+                oddIndex--;
+            }
+        }
+
+        return Convert.ToInt32(output);
+    }
+}
